044fb1673e298942fada757fde3841757222ac99
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-10-23 18:54:22 -0400

    addded notes

--- ex13.py
+++ ex13.py
@@ -1,9 +1,18 @@
+#tells program to take input and save to plug into scirpt (scirpt being the actual program)
 from sys import argv
-#read the WYSS section for how to run this
+
+#tells program the first input will be the script name, 1,2,3 will all ve inputs
 script, first, second, third = argv
-print("The script is called:", script)
+
+#shows what the name of the .py is
+print("This script is called:", script)
+
+#read the WYSS section for how to run this
+#each of these will display what the user entered for the three inputs
 print("Your first variable is:", first)
-print("Your secind variable is:", second)
+print("Your second variable is:", second)
 print("Your third variable is:", third)
+
+#prompts user to enter color and food
 input_1 = input('Enter color')
 input_2 = input('Enter food')
